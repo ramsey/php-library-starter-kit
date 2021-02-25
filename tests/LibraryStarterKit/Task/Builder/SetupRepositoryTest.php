2fe17ef43efa7c55d600b747ebc9a7bce4d2d2bd
Ben Ramsey <ben@benramsey.com>
2021-02-24 18:37:34 -0600

    ci: fix coding standards issue

--- tests/LibraryStarterKit/Task/Builder/SetupRepositoryTest.php
+++ tests/LibraryStarterKit/Task/Builder/SetupRepositoryTest.php
@@ -64,7 +64,13 @@ class SetupRepositoryTest extends TestCase
 
         $environment
             ->expects()
-            ->getProcess(['git', 'commit', '-n', '-m', 'chore: initialize project using ramsey/php-library-starter-kit'])
+            ->getProcess([
+                'git',
+                'commit',
+                '-n',
+                '-m',
+                'chore: initialize project using ramsey/php-library-starter-kit',
+            ])
             ->andReturn($processMustRunWithCallable);
 
         /** @var Build & MockInterface $build */
