40830dc218b4d24bf27c12cbb9a71c3d3d8f9997
Ben Ramsey <ben@benramsey.com>
2023-04-26 19:27:31 -0500

    chore: polish generated header files

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Vendor\Test\SubNamespace;
 
-use Mockery\MockInterface;
 use Vendor\SubNamespace\Example;
 
 class ExampleTest extends TestCase
 {
     public function testGreet(): void
     {
-        /** @var Example & MockInterface $example */
         $example = $this->mockery(Example::class);
         $example->shouldReceive('greet')->passthru();
 
