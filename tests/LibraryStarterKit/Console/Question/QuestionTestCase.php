0d35b751f83de00703f8bddedf58b7c51ab9bdda
Ben Ramsey <ben@ramsey.dev>
2025-03-06 21:38:09 -0600

    fix: partially revert dbec32c337e4bab527ca829db3d8cef3e7f08536
    This fixes a fatal error condition that occurs in some cases because
    Symfony's Question::getQuestion() method was not compatible with
    StarterKitQuestion::getQuestion(). Rather than attempt to specify the
    full Symfony Question interface as the StarterKitQuestion interface,
    this simply ignores the PHPStan errors in the QuestionTestCase test
    class.

--- tests/LibraryStarterKit/Console/Question/QuestionTestCase.php
+++ tests/LibraryStarterKit/Console/Question/QuestionTestCase.php
@@ -6,11 +6,12 @@ namespace Ramsey\Test\Dev\LibraryStarterKit\Console\Question;
 
 use Ramsey\Dev\LibraryStarterKit\Console\Question\StarterKitQuestion;
 use Ramsey\Test\Dev\LibraryStarterKit\TestCase;
+use Symfony\Component\Console\Question\Question;
 
 abstract class QuestionTestCase extends TestCase
 {
     /**
-     * @return class-string<StarterKitQuestion>
+     * @return class-string<StarterKitQuestion & Question>
      */
     abstract protected function getTestClass(): string;
 
@@ -26,6 +27,8 @@ abstract class QuestionTestCase extends TestCase
     public function testGetName(): void
     {
         $questionClass = $this->getTestClass();
+
+        /** @phpstan-ignore-next-line */
         $question = new $questionClass($this->answers);
 
         $this->assertSame($this->getQuestionName(), $question->getName());
@@ -34,6 +37,8 @@ abstract class QuestionTestCase extends TestCase
     public function testGetQuestion(): void
     {
         $questionClass = $this->getTestClass();
+
+        /** @phpstan-ignore-next-line */
         $question = new $questionClass($this->answers);
 
         $this->assertSame($this->getQuestionText(), $question->getQuestion());
@@ -42,6 +47,8 @@ abstract class QuestionTestCase extends TestCase
     public function testGetAnswers(): void
     {
         $questionClass = $this->getTestClass();
+
+        /** @phpstan-ignore-next-line */
         $question = new $questionClass($this->answers);
 
         $this->assertSame($this->answers, $question->getAnswers());
@@ -50,6 +57,8 @@ abstract class QuestionTestCase extends TestCase
     public function testGetDefault(): void
     {
         $questionClass = $this->getTestClass();
+
+        /** @phpstan-ignore-next-line */
         $question = new $questionClass($this->answers);
 
         $this->assertSame($this->getQuestionDefault(), $question->getDefault());
@@ -60,6 +69,8 @@ abstract class QuestionTestCase extends TestCase
         $this->answers->{$this->getQuestionName()} = 'foobar';
 
         $questionClass = $this->getTestClass();
+
+        /** @phpstan-ignore-next-line */
         $question = new $questionClass($this->answers);
 
         $this->assertSame('foobar', $question->getDefault());
